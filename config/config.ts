3021182c19379552e6f67455ce8ca7025e7279ef
chenxianjun <497514482@qq.com>
2026-04-24 11:13:44 +0800

    fix: 修改系统名称

--- config/config.ts
+++ config/config.ts
@@ -100,7 +100,7 @@ export default defineConfig({
    * @name layout 插件
    * @doc https://umijs.org/docs/max/layout-menu
    */
-  title: 'Ant Design Pro',
+  title: '智能体协同平台 v1.0',
   layout: {
     locale: true,
     ...defaultSettings,
--- config/defaultSettings.ts
+++ config/defaultSettings.ts
@@ -15,7 +15,7 @@ const Settings: ProLayoutProps & {
   fixedHeader: false,
   fixSiderbar: true,
   colorWeak: false,
-  title: 'Ant Design Pro',
+  title: '智能体协同平台 v1.0',
   pwa: true,
   logo: 'https://gw.alipayobjects.com/zos/rmsportal/KDpgvguMpGfqaHPjicRK.svg',
   iconfontUrl: '',
